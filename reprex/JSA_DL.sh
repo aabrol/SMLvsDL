734589188ac46bc4219acc90a75fdd1e34e89ca7
paul-reiners <paul.reiners@gmail.com>
2022-03-11 13:56:48 -0600

    Can load data.

--- reprex/JSA_DL.sh
+++ reprex/JSA_DL.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 #SBATCH -N 1 
 #SBATCH -n 2
-#SBATCH -p qTRDGPUL
+#SBATCH -p v100
 #SBATCH --gres=gpu:1
 #SBATCH -c 4
 #SBATCH --mem-per-cpu=4000
-#SBATCH -t 7200
+#SBATCH -t 24:00:00
 #SBATCH -J DL
 #SBATCH -e ../slogs/err%A-%a.err
 #SBATCH -o ../slogs/out%A-%a.out
-#SBATCH -A PSYC0002
+#SBATCH -A feczk001
 #SBATCH --oversubscribe 
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=aabrol@gsu.edu
+#SBATCH --mail-user=reiners@umn.edu
 
 sleep 5s
 
 export OMP_NUM_THREADS=1
 export MODULEPATH=/apps/Compilers/modules-3.2.10/Debug-Build/Modules/3.2.10/modulefiles/
 
-source /home/users/aabrol/anaconda3/bin/activate
-conda activate AA_DL
+source /home/miran045/reine097/projects/AlexNet_Abrol2021/venv/bin/activate
 
 ./run_DL.sh
 
