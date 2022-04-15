28201db4df22384bfdad8af0279cf3ca66423362
paul-reiners <paul.reiners@gmail.com>
2022-04-15 09:04:06 -0500

    Starting to get this thing working.

---
+++ src/dcan/dsets.py
@@ -0,0 +1,162 @@
+import copy
+import csv
+import functools
+import glob
+import os
+import random
+
+from collections import namedtuple
+
+import SimpleITK as sitk
+import numpy as np
+
+import torch
+import torch.cuda
+from torch.utils.data import Dataset
+import nibabel as nib
+
+from util.disk import getCache
+from util.logconf import logging
+
+log = logging.getLogger(__name__)
+# log.setLevel(logging.WARN)
+# log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
+
+raw_cache = getCache('dcan_raw')
+
+CandidateInfoTuple = namedtuple(
+    'CandidateInfoTuple',
+    'series_uid, eid_str, ses_str, age_int, sex_str, t1_t2_str, run_x_try_int, smriPath_str, motionQCscore_int, passfail_str, notes_str'
+)
+
+@functools.lru_cache(1)
+def getCandidateInfoList(requireOnDisk_bool=True):
+    # We construct a set with all series_uids that are present on disk.
+    # This will let us use the data, even if we haven't downloaded all of
+    # the subsets yet.
+    mri_list = glob.glob('data-unversioned/abcd/*.nii.gz')
+    presentOnDisk_set = {os.path.split(p)[-1][:-6] for p in mri_list}
+
+    candidateInfo_list = []
+    with open('data/qc_with_paths.csv', "r") as f:
+        for row in list(csv.reader(f))[1:]:
+            eid_ses_t1_t2_run_x_try_uid = '_'.join([row[0], row[1], row[4], row[5]])
+
+            if eid_ses_t1_t2_run_x_try_uid not in presentOnDisk_set and requireOnDisk_bool:
+                continue
+
+            eid_str = row[1]
+            ses_str = row[2]
+            age_int = int(row[3])
+            sex_str = row[4]
+            t1_t2_str = row[5]
+            run_x_try_int = int(row[6])
+            smriPath_str = row[7]
+            motionQCscore_int = int(row[8])
+            passfail_str = row[9]
+            notes_str = row[10]
+
+            candidateInfo_list.append(CandidateInfoTuple(
+                eid_ses_t1_t2_run_x_try_uid,
+                eid_str,
+                ses_str,
+                age_int,
+                sex_str,
+                t1_t2_str,
+                run_x_try_int,
+                smriPath_str,
+                motionQCscore_int,
+                passfail_str,
+                notes_str,
+            ))
+
+    candidateInfo_list.sort(reverse=True)
+    return candidateInfo_list
+
+class Mri:
+    def __init__(self, smri_path):
+        mri_path = glob.glob(
+            'data-unversioned/{}'.format(smri_path)
+        )[0]
+
+        mri_nii_gz = nib.load(mri_path)
+        mri_a = np.array(mri_nii_gz.get_fdata(), dtype=np.float32)
+
+        self.smri_path = smri_path
+        self.hu_a = mri_a
+
+    def getRawCandidate(self):
+        mri_chunk = self.hu_a
+
+        return mri_chunk
+
+
+@functools.lru_cache(1, typed=True)
+def getMri(series_uid):
+    return Mri(series_uid)
+
+
+@raw_cache.memoize(typed=True)
+def getCtRawCandidate(series_uid, center_xyz, width_irc):
+    mri = getMri(series_uid)
+    mri_chunk = mri.getRawCandidate()
+    return mri_chunk
+
+
+class ABCDDataset(Dataset):
+    def __init__(self,
+                 val_stride=0,
+                 isValSet_bool=None,
+                 series_uid=None,
+                 sortby_str='random',
+            ):
+        self.candidateInfo_list = copy.copy(getCandidateInfoList())
+
+        if series_uid:
+            self.candidateInfo_list = [
+                x for x in self.candidateInfo_list if x.series_uid == series_uid
+            ]
+
+        if isValSet_bool:
+            assert val_stride > 0, val_stride
+            self.candidateInfo_list = self.candidateInfo_list[::val_stride]
+            assert self.candidateInfo_list
+        elif val_stride > 0:
+            del self.candidateInfo_list[::val_stride]
+            assert self.candidateInfo_list
+
+        if sortby_str == 'random':
+            random.shuffle(self.candidateInfo_list)
+        elif sortby_str == 'series_uid':
+            self.candidateInfo_list.sort(key=lambda x: (x.series_uid, x.center_xyz))
+        elif sortby_str == 'label_and_size':
+            pass
+        else:
+            raise Exception("Unknown sort: " + repr(sortby_str))
+
+        log.info("{!r}: {} {} samples".format(
+            self,
+            len(self.candidateInfo_list),
+            "validation" if isValSet_bool else "training",
+        ))
+
+    def __len__(self):
+        return len(self.candidateInfo_list)
+
+    def __getitem__(self, ndx):
+        candidateInfo_tup = self.candidateInfo_list[ndx]
+
+        candidate_a = getCtRawCandidate(
+            candidateInfo_tup.series_uid,
+        )
+        candidate_t = torch.from_numpy(candidate_a).to(torch.float32)
+        candidate_t = candidate_t.unsqueeze(0)
+
+        age_t = torch.tensor([
+                not candidateInfo_tup.age_int
+            ],
+            dtype=torch.int
+        )
+
+        return candidate_t, age_t, candidateInfo_tup.series_uid
--- src/dcan/model.py
+++ src/dcan/model.py
@@ -1,33 +1,88 @@
-import tensorflow as tf
+import math
+
+from torch import nn as nn
+
+from util.logconf import logging
+
+log = logging.getLogger(__name__)
+# log.setLevel(logging.WARN)
+# log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
 
 
-def create_model(input_shape):
-    # See https://towardsdatascience.com/implementing-alexnet-cnn-architecture-using-tensorflow-2-0-and-keras-2113e090ad98
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv3D(
-            filters=96, kernel_size=(11, 11, 11), strides=(4, 4, 4), activation='relu', input_shape=input_shape),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.MaxPool3D(pool_size=(3, 3, 3), strides=(2, 2, 2)),
-        tf.keras.layers.Conv3D(
-            filters=256, kernel_size=(5, 5, 5), strides=(1, 1, 1), activation='relu', padding="same"),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.MaxPool3D(pool_size=(3, 3, 3), strides=(2, 2, 2)),
-        tf.keras.layers.Conv3D(
-            filters=384, kernel_size=(3, 3, 3), strides=(1, 1, 1), activation='relu', padding="same"),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Conv3D(
-            filters=384, kernel_size=(3, 3, 3), strides=(1, 1, 1), activation='relu', padding="same"),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Conv3D(
-            filters=256, kernel_size=(3, 3, 3), strides=(1, 1, 1), activation='relu', padding="same"),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.MaxPool3D(pool_size=(3, 3, 3), strides=(2, 2, 2)),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(4096, activation='relu'),
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(4096, activation='relu'),
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(64, activation='relu')
-    ])
+class LunaModel(nn.Module):
+    def __init__(self, in_channels=1, conv_channels=8):
+        super().__init__()
 
-    return model
+        self.tail_batchnorm = nn.BatchNorm3d(1)
+
+        self.block1 = LunaBlock(in_channels, conv_channels)
+        self.block2 = LunaBlock(conv_channels, conv_channels * 2)
+        self.block3 = LunaBlock(conv_channels * 2, conv_channels * 4)
+        self.block4 = LunaBlock(conv_channels * 4, conv_channels * 8)
+
+        self.head_linear = nn.Linear(1152, 2)
+        self.head_softmax = nn.Softmax(dim=1)
+
+        self._init_weights()
+
+    # see also https://github.com/pytorch/pytorch/issues/18182
+    def _init_weights(self):
+        for m in self.modules():
+            if type(m) in {
+                nn.Linear,
+                nn.Conv3d,
+                nn.Conv2d,
+                nn.ConvTranspose2d,
+                nn.ConvTranspose3d,
+            }:
+                nn.init.kaiming_normal_(
+                    m.weight.data, a=0, mode='fan_out', nonlinearity='relu',
+                )
+                if m.bias is not None:
+                    fan_in, fan_out = \
+                        nn.init._calculate_fan_in_and_fan_out(m.weight.data)
+                    bound = 1 / math.sqrt(fan_out)
+                    nn.init.normal_(m.bias, -bound, bound)
+
+
+
+    def forward(self, input_batch):
+        bn_output = self.tail_batchnorm(input_batch)
+
+        block_out = self.block1(bn_output)
+        block_out = self.block2(block_out)
+        block_out = self.block3(block_out)
+        block_out = self.block4(block_out)
+
+        conv_flat = block_out.view(
+            block_out.size(0),
+            -1,
+        )
+        linear_output = self.head_linear(conv_flat)
+
+        return linear_output, self.head_softmax(linear_output)
+
+
+class LunaBlock(nn.Module):
+    def __init__(self, in_channels, conv_channels):
+        super().__init__()
+
+        self.conv1 = nn.Conv3d(
+            in_channels, conv_channels, kernel_size=3, padding=1, bias=True,
+        )
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv3d(
+            conv_channels, conv_channels, kernel_size=3, padding=1, bias=True,
+        )
+        self.relu2 = nn.ReLU(inplace=True)
+
+        self.maxpool = nn.MaxPool3d(2, 2)
+
+    def forward(self, input_batch):
+        block_out = self.conv1(input_batch)
+        block_out = self.relu1(block_out)
+        block_out = self.conv2(block_out)
+        block_out = self.relu2(block_out)
+
+        return self.maxpool(block_out)
