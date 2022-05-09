3af0adcb36b32d635360ec43394f1fda942b83e1
paul-reiners <paul.reiners@gmail.com>
2022-05-09 09:34:47 -0500

    Generalized method.

--- src/dcan/plot/create_plot_from_log.py
+++ src/dcan/plot/create_plot_from_log.py
@@ -3,7 +3,8 @@
 import matplotlib.pyplot as plt
 import sys
 
-def create_loss_plot(file, metric1, metric1_label, metric2, metric2_label):
+
+def create_plot_from_log(file, metric1, metric1_label, metric2, metric2_label):
     file = open(file, 'r')
     lines = file.readlines()
 
@@ -16,15 +17,16 @@ def create_loss_plot(file, metric1, metric1_label, metric2, metric2_label):
 
         return metric
 
-    training_losses = [get_metric_from_line(line) for line in training_lines]
-    validation_losses = [get_metric_from_line(line) for line in validation_lines]
+    training_vals = [get_metric_from_line(line) for line in training_lines]
+    validation_vals = [get_metric_from_line(line) for line in validation_lines]
 
     epochs = range(100)
-    plt.plot(epochs, training_losses, label=metric1_label)
-    plt.plot(epochs, validation_losses, label=metric2_label)
+    plt.plot(epochs, training_vals, label=metric1_label)
+    plt.plot(epochs, validation_vals, label=metric2_label)
     plt.legend()
     plt.show()
 
+
 if __name__ == '__main__':
     args = sys.argv[1:]
-    create_loss_plot(args[0], args[1], args[2], args[3], args[4])
+    create_plot_from_log(args[0], args[1], args[2], args[3], args[4])
