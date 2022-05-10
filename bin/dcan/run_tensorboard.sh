c0c5d97fb54b426dcde9a12096d1bb6e71a9bff7
paul-reiners <paul.reiners@gmail.com>
2022-05-10 13:36:18 -0500

    Have tensorflow working.

--- bin/dcan/run_tensorboard.sh
+++ bin/dcan/run_tensorboard.sh
@@ -1,1 +1,4 @@
-/home/miran045/reine097/.local/bin/tensorboard $1
+cd /home/miran045/reine097/projects/AlexNet_Abrol2021
+/home/miran045/reine097/.local/bin/tensorboard --logdir runs/ --host localhost --port 8088
+
+% http://localhost:8088
